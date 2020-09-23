55087b8d47d3e96fb7865764d84f83eeb93f44c8
Alex Albu <alex.albu@gmail.com>
2020-09-23 13:05:45 -0400

    Fix Parquet encoding for timestamps before epoch

--- src/main/java/org/apache/hadoop/hive/ql/io/parquet/timestamp/NanoTimeUtils.java
+++ src/main/java/org/apache/hadoop/hive/ql/io/parquet/timestamp/NanoTimeUtils.java
@@ -2,6 +2,7 @@ package org.apache.hadoop.hive.ql.io.parquet.timestamp;
 
 import org.apache.hadoop.hive.common.type.Timestamp;
 
+import static java.lang.Math.floorDiv;
 import static java.lang.Math.toIntExact;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -10,13 +11,15 @@ public final class NanoTimeUtils
     private NanoTimeUtils() {}
 
     private static final int JULIAN_EPOCH_OFFSET_DAYS = 2_440_588;
+    private static final long SECONDS_PER_DAY = 86400L;
 
     public static NanoTime getNanoTime(Timestamp timestamp, @SuppressWarnings("unused") boolean ignored)
     {
-        int epochDay = toIntExact(SECONDS.toDays(timestamp.toEpochSecond()));
+        long epochSeconds = timestamp.toEpochSecond();
+        int epochDay = toIntExact(floorDiv(epochSeconds, SECONDS_PER_DAY));
         int julianDay = JULIAN_EPOCH_OFFSET_DAYS + epochDay;
 
-        long timeOfDaySeconds = timestamp.toEpochSecond() % 86400;
+        long timeOfDaySeconds = epochSeconds - epochDay * SECONDS_PER_DAY;
         long timeOfDayNanos = SECONDS.toNanos(timeOfDaySeconds) + timestamp.getNanos();
 
         return new NanoTime(julianDay, timeOfDayNanos);
