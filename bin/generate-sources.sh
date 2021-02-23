2423c689ba4875ad23faafcb54c3463c9de900c3
David Phillips <david@acz.org>
2021-02-23 09:00:37 -0800

    Revert change to Thrift generation

--- bin/generate-sources.sh
+++ bin/generate-sources.sh
@@ -9,6 +9,6 @@ SOURCE_DIR=src/main/thrift
 TARGET_DIR=target/generated-sources/thrift
 rm -rf "${ROOT_DIR:?}/${TARGET_DIR}"
 mkdir -p "${ROOT_DIR}/${TARGET_DIR}"
-docker run -u "$(id -u)" -v "${ROOT_DIR}:/rundir" thrift:0.9.3 thrift -o /rundir/${TARGET_DIR} --gen java:beans,generated_annotations=suppress /rundir/${SOURCE_DIR}/hive_metastore.thrift
+docker run -u "$(id -u)" -v "${ROOT_DIR}:/rundir" thrift:0.9.3 thrift -o /rundir/${TARGET_DIR} --gen java:beans,hashcode,generated_annotations=undated /rundir/${SOURCE_DIR}/hive_metastore.thrift
 mv "${ROOT_DIR}/${TARGET_DIR}"/gen-javabean/* "${ROOT_DIR}/${TARGET_DIR}"
 rmdir "${ROOT_DIR}/${TARGET_DIR}"/gen-javabean
